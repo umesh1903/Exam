0af410ad0afc2c06ac4f0e990edcd116201069d8
umesh1903 <uu6463259@gmail.com>
2025-01-27 09:28:30 +0530

    added

---
+++ App.js
@@ -0,0 +1,6 @@
+const readline = require("readline").createInterface;
+function createInterface(input:NodeJS.ReadableStream,output?:NodeJS.WritableStream,
+completr?:Completer | import("readline").AsyncCompleter, terminal boolean
+)
+
+
